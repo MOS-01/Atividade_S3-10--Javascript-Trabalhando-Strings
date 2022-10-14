3ddf1ca17654d01e30b704f437f6f53ce8b39ee5
Mayza <mayza.santos@outlook.com.br>
2022-10-14 08:37:48 -0300

    fist commit

---
+++ comparacoes.js
@@ -0,0 +1,43 @@
+//Exercício 1
+/*let valor1 = 10
+let valor2 = '10'
+let compracaoDeValores = valor1===valor2
+
+console.log(compracaoDeValores)
+
+//Exercício 2
+let usuario1Altura = 1.80
+let usuario2Altura = 1.75
+let compracaoDeAlturas = usuario1Altura>usuario2Altura
+
+console.log(compracaoDeAlturas)
+
+//Exercício 3
+let valor1 = 10
+let valor2 = '10'
+let compracaoDeValores = valor1!==valor2
+
+console.log(compracaoDeValores)
+
+let idade = 15
+
+if (idade >= 16) {
+    console.log (`Obrigada,pela compra`)
+} else {
+    console.log (`Você não tem idade suficiente`)
+}
+
+let x = 'Fanta'
+let y = 'Convenção'
+
+if (x){
+    console.log (`Comprei o refrigerante ${x}.`)
+} else if (y) {
+    console.log(`Comprei o refrigerante ${y}.`)
+
+}else {
+    console.log('Não tinha nenhum')
+}*/
+
+
+ 
---
+++ index.js
@@ -0,0 +1,18 @@
+//Exercício 1
+
+let meuNome = 'Mayza'
+let comprimentoMeuNome = meuNome.length
+console.log(`O meu nome possui ${comprimentoMeuNome} caracteres de comprimento.`)
+
+//Exercício 2
+let preco = 15
+let precoFormarado = 'R$15,00'
+console.log (`O valor ${preco} formatado ficaria assim: ${precoFormarado}`)
+
+//Exercício 3
+let cumprimento = 'Bom dia'
+let nome = 'Mayza'
+let sobrenome = 'Santos'
+let mensagemCumprimento = (`${cumprimento}, ${nome} ${sobrenome}`)
+
+console.log(mensagemCumprimento)
